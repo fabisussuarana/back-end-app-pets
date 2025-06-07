b8fda68fdec5cc02ecfdd045f4bef5424028730a
Antonio Andre <antonioandre1008@gmail.com>
2025-06-06 21:53:55 -0500

    feat: atualiza seeders com dados reais

--- back-end-pets/seeders/20250513032234-demo-dicas.js
+++ back-end-pets/seeders/20250513032234-demo-dicas.js
@@ -1,38 +1,11 @@
 export const up = async (queryInterface, Sequelize) => {
   await queryInterface.bulkInsert("dicas", [
     {
-      titulo: "Como manter seu pet hidratado",
-      descricao: "Deixe sempre potes com água limpa e fresca em locais acessíveis, principalmente nos dias mais quentes. Fontes de água corrente também são ótimas opções para estimular o consumo.",
-      status: "ativo",
-      imagem: "https://images.unsplash.com/photo-1583511666445-775f1f2116f5",
-      id_usuario: 1, // Relacionando ao usuário João
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      titulo: "Escovação adequada dos pelos",
-      descricao: "Escove seu pet regularmente para remover pelos soltos e evitar nós. Isso também ajuda a reduzir a quantidade de pelos espalhados pela casa.",
-      status: "ativo",
-      imagem: "https://images.unsplash.com/photo-1601758123927-196edb17b5a1",
-      id_usuario: 2, // Relacionando ao usuário Maria
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
       titulo: "Alimentação saudável para cães e gatos",
       descricao: "Ofereça rações de qualidade, evite alimentos humanos e consulte sempre um veterinário antes de mudar a dieta. Petiscos naturais são boas opções de agrado.",
       status: "ativo",
-      imagem: "https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2",
-      id_usuario: 3, // Relacionando ao usuário Carlos
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      titulo: "Enriquecimento ambiental para pets",
-      descricao: "Brinquedos interativos, túneis e esconderijos ajudam a manter seu pet mentalmente estimulado e feliz. Enriquecimento é essencial, principalmente para pets que ficam sozinhos.",
-      status: "ativo",
-      imagem: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee",
-      id_usuario: 4, // Relacionando ao usuário Ana
+      imagem: "https://images.unsplash.com/photo-1568640347023-a616a30bc3bd?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id_usuario: 1, 
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -40,8 +13,8 @@ export const up = async (queryInterface, Sequelize) => {
       titulo: "Passeios seguros com seu cão",
       descricao: "Use coleira adequada, evite horários muito quentes, leve saquinhos para recolher as fezes e esteja atento ao ambiente ao redor para garantir a segurança.",
       status: "ativo",
-      imagem: "https://images.unsplash.com/photo-1608137681981-d66d8f49b6c1",
-      id_usuario: 5, // Relacionando ao usuário Pedro
+      imagem: "https://images.unsplash.com/photo-1560743641-3914f2c45636?q=80&w=1851&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id_usuario: 2, 
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -49,8 +22,8 @@ export const up = async (queryInterface, Sequelize) => {
       titulo: "Importância da vacinação em dia",
       descricao: "Vacinas protegem seu pet contra diversas doenças graves. Mantenha o cartão de vacinação atualizado e siga as orientações do veterinário.",
       status: "ativo",
-      imagem: "https://images.unsplash.com/photo-1619983081563-b64f2d59f3b4",
-      id_usuario: 1, // Relacionando ao usuário João
+      imagem: "https://plus.unsplash.com/premium_photo-1710406095492-7e62eba19745?q=80&w=2102&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id_usuario: 3, 
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -59,7 +32,7 @@ export const up = async (queryInterface, Sequelize) => {
       descricao: "Crie um ambiente seguro e silencioso para seu pet. Use sons calmantes, feromônios sintéticos e consulte um veterinário sobre opções de tratamento.",
       status: "ativo",
       imagem: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d",
-      id_usuario: 2, // Relacionando ao usuário Maria
+      id_usuario: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -68,7 +41,7 @@ export const up = async (queryInterface, Sequelize) => {
       descricao: "Escove os dentes do seu pet com produtos específicos e ofereça brinquedos mastigáveis. A saúde bucal evita problemas mais sérios no futuro.",
       status: "ativo",
       imagem: "https://images.unsplash.com/photo-1578574577315-3fbeb0cecdc2",
-      id_usuario: 3, // Relacionando ao usuário Carlos
+      id_usuario: 3, 
       createdAt: new Date(),
       updatedAt: new Date(),
     }
